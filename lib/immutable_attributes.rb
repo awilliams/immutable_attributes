0b43a18fafd1d03087a86e66a8cb1079db226537
Adam Williams <adam.williams@traddia.com>
2011-06-22 14:09:59 +0200

    added immutable? method

--- lib/immutable_attributes.rb
+++ lib/immutable_attributes.rb
@@ -13,6 +13,8 @@ module ImmutableAttributes
   VERSION = "1.0.3"
 
   class Immutable < Set
+    attr_accessor :config
+
     # allow setting attribute as mutable within block
     def make_mutable(*attr_names)
       self.subtract(attr_names)
@@ -43,7 +45,9 @@ module ImmutableAttributes
       config.update(attr_names.extract_options!)
 
       @immutables ||= Immutable.new
+      @immutables.config ||= {}
       @immutables.merge(attr_names)
+      attr_names.each { |attr| @immutables.config[attr] = config }
 
       instance_eval do
         protected
@@ -53,6 +57,18 @@ module ImmutableAttributes
       end
 
       class_eval do
+        def immutable?(attr, callbacks = true)
+          attr = attr.to_sym
+          callbacks_results = if callbacks
+            config_if = self.immutables.config[attr].fetch(:if, lambda {|x| true}).call(self)
+            config_unless = self.immutables.config[attr].fetch(:unless, lambda {|x| false}).call(self)
+            config_if && !config_unless
+          else
+            true
+          end
+          self.immutables.include?(attr) && callbacks_results
+        end
+
         def immutables
           # each instance maintains it's own copy of the immutable set
           @immutables ||= self.class.send(:immutables).dup
@@ -60,7 +76,7 @@ module ImmutableAttributes
       end
 
       validates_each(attr_names, config) do |record, attr_name, value|
-        record.errors.add(attr_name, config[:message]) if record.changed.include?(attr_name.to_s) && record.immutables.include?(attr_name)
+        record.errors.add(attr_name, config[:message]) if record.changed.include?(attr_name.to_s) && record.immutable?(attr_name, false)
       end
     end
 
